500e096fe84c417912584f1017e6158e4e56e1c9
magicsli <2544510157@qq.com>
2021-03-05 13:53:49 +0800

    feat: input-number

---
+++ el-number/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-input type="text" v-bind="$attrs" v-on="{...$listeners, input:handleInput }" />
+</template>
+
+<script>
+export default {
+  name: 'ElNumber',
+  methods: {
+    handleInput(event) {
+      console.log(event)
+      const inputValue = event.replace(/[^0-9\.]/g, '')
+      this.$emit('input', inputValue)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+/*在谷歌下移除input[number]的上下箭头*/
+ /deep/ input[type='number']::-webkit-outer-spin-button,
+ /deep/ input[type='number']::-webkit-inner-spin-button{
+    -webkit-appearance: none !important;
+}
+/*在firefox下移除input[number]的上下箭头*/
+/deep/ input[type="number"]{
+    -moz-appearance:textfield;
+}
+</style>
